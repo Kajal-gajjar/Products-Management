99011198565e18a24753653b09814a0986e4b5c5
ravikrpandey <104829126+ravikrpandey@users.noreply.github.com>
2022-07-25 12:26:15 +0530

    get api created

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -16,6 +16,25 @@ const loginUser = async function (req, res) {
 
 const getUserProfile = async function (req, res) {
   try {
+    let filters = req.query
+
+    Object.keys(filters).forEach(x => filters[x] = filters[x].trim())
+
+    if(Object.keys(filters).length === 0) { // This block will work to fetch all books incase no filter is provided
+
+      let user = await userModel.find({isDeleted: false, isPublic: true})
+      if(user.length === 0)res.status(404).send({ status: false, message: "User not found" })
+      res.status(200).send({ status: true, data: user })
+
+    } else {
+
+      filters.isDeleted = false
+      filters.isPublic = true
+
+      let filteredUser = await userModel.find(filters)
+            if (filteredUser.length === 0) return res.status(404).send({ status: false, msg: "No such data available" })
+            else return res.status(200).send({ status: true, data: filteredUser })
+    }
   } catch (err) {
     return res.status(500).send({ status: false, message: err.message });
   }
